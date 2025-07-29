501213caccb207330a67eceaeda2122c7786860a
Abelis Vladimir <vova.abelis@yandex.ru>
2025-07-29 21:58:55 +0300

    dfdfd

--- Assets/Scripts/sasa.cs
+++ Assets/Scripts/sasa.cs
@@ -7,7 +7,7 @@ public class sasa : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("sasa start");
     }
 
     // Update is called once per frame
